f983723ed97897aa6e0a784766d0824ec0e8de7c
YanYMO <yanmartinsd3@gmail.com>
2026-06-17 16:11:32 -0300

    feat: Atualizando os estilos do componente da lista de favoritos e pequenos ajustes.

--- src/components/ListaFavoritos/index.tsx
+++ src/components/ListaFavoritos/index.tsx
@@ -2,11 +2,12 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 export const ListaFavoritos = () => {
-  
+
   return (
     <TouchableOpacity style={styles.card} activeOpacity={0.85}>
       <View style={styles.conteudo}>
-        <Text>Opa</Text>
+        <Text style={styles.cidadeTexto}>Rio de Janeiro</Text>
+        <Text style={styles.tempTexto}>16º</Text>
       </View>
       <TouchableOpacity activeOpacity={0.8} style={styles.botaoRemover}>
         <Text style={styles.botaoRemoverTexto}>X</Text>
--- src/components/ListaFavoritos/styles.ts
+++ src/components/ListaFavoritos/styles.ts
@@ -1,14 +1,22 @@
 import { StyleSheet } from "react-native";
 
+const whiteColor: string = "#fff";
+
+const cardBorderColor: string = "rgba(217, 217, 217, 0.55)";
+const backgroundColor: string = "rgb(5, 102, 141)";
+
+const cardAlertBackgroundColor: string = "rgba(204, 88, 3,0.8)";
+const cardBackgroundColor: string = "rgba(217, 217, 217, 0.13)";
+
 export const styles = StyleSheet.create({
   /* estilização padrão dos cards */
   card: {
-    backgroundColor: "cardBackgroundColor",
+    backgroundColor: cardBackgroundColor,
     width: "100%",
     height: "100%",
     alignItems: "center",
     alignContent: "center",
-    borderColor: "cardBorderColor",
+    borderColor: cardBorderColor,
     borderRadius: 16,
     borderWidth: 1,
     maxHeight: 80,
@@ -17,6 +25,16 @@ export const styles = StyleSheet.create({
   },
   conteudo: {
     padding: 16,
+  },
+  cidadeTexto: {
+    color: whiteColor,
+    fontWeight: "600", 
+    fontSize: 16
+  },
+  tempTexto: {
+    fontSize: 30,
+    fontWeight: "600",
+    color: whiteColor,
   },
   botaoRemover: {
     padding: 16,
@@ -28,7 +46,7 @@ export const styles = StyleSheet.create({
     justifyContent: "center",
   },
   botaoRemoverTexto: {
-    color: "white",
+    color: whiteColor,
     fontSize: 24,
     fontWeight: 800,
     textAlign: "center",
