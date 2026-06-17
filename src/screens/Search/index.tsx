749d949c7d3e8196da7b7bd1936841c2b691401a
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-06-17 20:17:31 -0300

    fix: retirando o botão de alerta

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,31 +1,15 @@
 import React, { useState } from "react";
-import { Image, Text, TouchableOpacity, View, TextInput} from "react-native";
+import { TextInput, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./styles";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import iconeAlerta from "../../../assets/Siren.png";
 
-export const Search = () => {
+export const SearchScreen = () => {
   const [notificacao, setNotificacao] = useState(true);
   const [search, setSearch] = useState("");
- 
-return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container} edges={["left", "right", "top"]}>
-        <View style={styles.header}>
-  <TouchableOpacity style={styles.backBtn}>
-    <Text style={styles.backArrow}>← <Text style= {styles.backText}> Voltar</Text></Text>
-    
-  </TouchableOpacity>
 
-  <TouchableOpacity activeOpacity={0.8}>
-    {notificacao && (
-      <View style={styles.iconeContainer}>
-        <Image source={iconeAlerta} />
-      </View>
-    )}
-  </TouchableOpacity>
-</View>
-      <View  style={styles.searchWrapper}>
+  return (
+    <SafeAreaView style={styles.container} edges={["left", "right", "top"]}>
+      <View style={styles.searchWrapper}>
         <TextInput
           style={styles.searchInput}
           placeholder="Pesquisar cidade..."
@@ -33,15 +17,8 @@ return (
           value={search}
           onChangeText={setSearch}
           autoCorrect={false}
-         />
-       </View>
-       </SafeAreaView>
-    </SafeAreaProvider>
-      );
-    };
-        
-        
-      
-      
-        
-
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
