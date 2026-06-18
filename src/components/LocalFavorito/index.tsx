9e793b30b42d0f93a5dc3e47ece11910319beee2
YanYMO <yanmartinsd3@gmail.com>
2026-06-17 21:54:06 -0300

    feat: Adicionando as funções para buscar do AsincStorage a lçista de locais favoritos. No momento utilizando uma lista personalizada, para testes.

--- src/components/LocalFavorito/index.tsx
+++ src/components/LocalFavorito/index.tsx
@@ -3,8 +3,10 @@ import { styles } from "./styles";
 
 export type Favorito = {
   id: string;
+  nomeCidade: string;
   longitude: string;
   latitude: string;
+  temperatura?: string;
 };
 
 type FavoritoProps = {
@@ -16,8 +18,8 @@ export function LocalFavorito({ local, removeFavorito }: FavoritoProps) {
   return (
     <TouchableOpacity style={styles.card} activeOpacity={0.85}>
       <View style={styles.conteudo}>
-        <Text style={styles.cidadeTexto}>{local.longitude}</Text>
-        <Text style={styles.tempTexto}>{local.latitude}</Text>
+        <Text style={styles.cidadeTexto}>{local.nomeCidade}</Text>
+        <Text style={styles.tempTexto}>{local.temperatura} ºC</Text>
       </View>
       <TouchableOpacity
         onPress={() => removeFavorito(local.id)}
