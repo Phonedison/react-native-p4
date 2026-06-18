065a2736ec6dad57064f3058c121247ee295ca2d
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-06-18 18:47:20 -0300

    fix: implmentando o método useBuscarClimaCidade no componente LocalFavorito

--- src/components/LocalFavorito/index.tsx
+++ src/components/LocalFavorito/index.tsx
@@ -1,4 +1,6 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { useEffect } from "react";
+import { ActivityIndicator, Text, TouchableOpacity, View } from "react-native";
+import { useBuscarClimaCidade } from "../../hooks";
 import { styles } from "./styles";
 
 export type Favorito = {
@@ -15,11 +17,29 @@ type FavoritoProps = {
 };
 
 export function LocalFavorito({ local, removeFavorito }: FavoritoProps) {
+  const { climaLocal, loading, buscarClimaLocal } = useBuscarClimaCidade();
+
+  useEffect(() => {
+    buscarClimaLocal(Number(local.latitude), Number(local.longitude));
+  }, [local.latitude, local.longitude]);
+
+  if (loading) {
+    return (
+      <View style={[styles.card, { padding: 20, justifyContent: "center" }]}>
+        <ActivityIndicator size="small" color="#000" />
+      </View>
+    );
+  }
+
+  if (!climaLocal) return null;
+
   return (
     <TouchableOpacity style={styles.card} activeOpacity={0.85}>
       <View style={styles.conteudo}>
         <Text style={styles.cidadeTexto}>{local.nomeCidade}</Text>
-        <Text style={styles.tempTexto}>{local.temperatura} ºC</Text>
+        <Text style={styles.tempTexto}>
+          {climaLocal.hourly?.temperature_2m?.[0]} ºC
+        </Text>
       </View>
       <TouchableOpacity
         onPress={() => removeFavorito(local.id)}
