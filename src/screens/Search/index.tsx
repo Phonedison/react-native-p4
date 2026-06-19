db4ade607354602431e4fcd9e4c959ba42823d06
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-06-18 21:29:32 -0300

    feat: componentizando elemento da Home

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,6 +1,12 @@
-
 import React, { useState, useEffect } from "react";
-import { TextInput, View, TouchableOpacity, Text, FlatList, ActivityIndicator } from "react-native";
+import {
+  TextInput,
+  View,
+  TouchableOpacity,
+  Text,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
@@ -11,32 +17,38 @@ import { SearchInput } from "../../components/SearchInput";
 import { EmptyState } from "../../components/EmptyState";
 import { CityResultItem } from "../../components/CityResultItem";
 
-
 type NavigationProp = StackNavigationProp<RootStackParamList, "SearchPage">;
 
 export const SearchScreen = () => {
   const navigation = useNavigation<NavigationProp>();
   const [search, setSearch] = useState("");
   const [favoritos, setFavoritos] = useState<Set<number>>(new Set());
-  const [temperaturas, setTemperaturas] = useState<Record<number, number | null>>({});
+  const [temperaturas, setTemperaturas] = useState<
+    Record<number, number | null>
+  >({});
 
-  const { locaisEncontrados, loading, buscarCidade, limparResultados, buscarTemperatura } = useBuscarClima();
-  
+  const {
+    locaisEncontrados,
+    loading,
+    buscarCidade,
+    limparResultados,
+    buscarTemperatura,
+  } = useBuscarClima();
+
   useEffect(() => {
-  if (locaisEncontrados.length === 0) {
-    setTemperaturas({});
-    return;
-  }
+    if (locaisEncontrados.length === 0) {
+      setTemperaturas({});
+      return;
+    }
 
-  locaisEncontrados.forEach(async (cidade) => {
-    const temp = await buscarTemperatura(cidade.latitude, cidade.longitude);
-    setTemperaturas(prev => ({ ...prev, [cidade.id]: temp }));
-  });
-}, [locaisEncontrados]);
-
+    locaisEncontrados.forEach(async (cidade) => {
+      const temp = await buscarTemperatura(cidade.latitude, cidade.longitude);
+      setTemperaturas((prev) => ({ ...prev, [cidade.id]: temp }));
+    });
+  }, [locaisEncontrados]);
 
   const handleFavoritar = (id: number) => {
-    setFavoritos(prev => {
+    setFavoritos((prev) => {
       const next = new Set(prev);
       next.has(id) ? next.delete(id) : next.add(id);
       return next;
@@ -44,52 +56,48 @@ export const SearchScreen = () => {
   };
 
   const renderItem = ({ item }: { item: any }) => (
-  <CityResultItem
-    item={item}
-    isFavorito={favoritos.has(item.id)}
-    temperatura={temperaturas[item.id]}
-    onPress={() => console.log("navegar para detalhes de", item.name)}
-    onFavoritar={() => handleFavoritar(item.id)}
-  />
-);
-return (
-  <SafeAreaProvider>
-    <SafeAreaView style={styles.container} edges={["left", "right", "top"]}>
-      <SearchInput
-         value={search}
-         onSearch={(text) => { setSearch(text); buscarCidade(text); }}
-         onClear={(text) => { setSearch(text); limparResultados(); }} 
-        />
-    {loading && (
-      <ActivityIndicator size="large" color="#fff" style={{ marginTop: 20 }} />
-      )}
-
-    <FlatList
-       data={locaisEncontrados}
-       keyExtractor={(item) => String(item.id)}
-       renderItem={renderItem}
-       keyboardShouldPersistTaps="handled"
-       showsVerticalScrollIndicator={false}
-       style={{ width: "100%" }}
-       ListEmptyComponent={
-    <EmptyState visible={search.trim().length >= 3 && !loading} />
-     }
+    <CityResultItem
+      item={item}
+      isFavorito={favoritos.has(item.id)}
+      temperatura={temperaturas[item.id]}
+      onPress={() => console.log("navegar para detalhes de", item.name)}
+      onFavoritar={() => handleFavoritar(item.id)}
     />
-    </SafeAreaView>
-  </SafeAreaProvider>
-);
+  );
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container} edges={["left", "right", "top"]}>
+        <SearchInput
+          value={search}
+          onSearch={(text) => {
+            setSearch(text);
+            buscarCidade(text);
+          }}
+          onClear={(text) => {
+            setSearch(text);
+            limparResultados();
+          }}
+        />
+        {loading && (
+          <ActivityIndicator
+            size="large"
+            color="#fff"
+            style={{ marginTop: 20 }}
+          />
+        )}
+
+        <FlatList
+          data={locaisEncontrados}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={renderItem}
+          keyboardShouldPersistTaps="handled"
+          showsVerticalScrollIndicator={false}
+          style={{ width: "100%" }}
+          ListEmptyComponent={
+            <EmptyState visible={search.trim().length >= 3 && !loading} />
+          }
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 };
-
-      
-
-
-        
-
-          
-        
-
-        
-      
-      
-        
-
